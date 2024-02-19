9b90c61caa4c4dc61357172f8a4f685239baad62
Kshitija <ksjadhav@iu.edu>
2024-02-18 22:17:34 -0500

    Chapter 2 and 3 commit

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
+import androidx.lifecycle.ViewModelProvider
+
 import com.example.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "MainActivity"
+private const val REQUEST_CODE_CHEAT = 0
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var quizViewModel: QuizViewModel
+    private lateinit var cheatButton: Button
+
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -27,10 +35,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
+        quizViewModel = ViewModelProvider(this)[QuizViewModel::class.java]
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        cheatButton = findViewById(R.id.cheat_button)
 //        setContentView(R.layout.activity_main)
 
+        if(quizViewModel.cCount > 0) {
+            binding.cheatNumber.text = "You have ${quizViewModel.cCount} attempts left"
+        }
         val questionResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionResId)
 
@@ -47,10 +60,36 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
-        binding.previousButton.setOnClickListener {
-            updateQuestion()
+//        binding.previousButton.setOnClickListener {
+//            updateQuestion()
+//        }
+        binding.cheatButton.setOnClickListener {
+            // Start CheatActivity
+            val answerIsTrue = questionBank[currentIndex].answer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
 
+
+    }
+    override fun onActivityResult(requestCode: Int,
+                                  resultCode: Int,
+                                  data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            quizViewModel.cCount -= 1
+            if(quizViewModel.cCount > 0) {
+                binding.cheatNumber.text = "You have ${quizViewModel.cCount} attempts left"
+            } else {
+                binding.cheatNumber.text = "You have no more attempts left"
+                binding.cheatButton.isEnabled = false
+            }
+            quizViewModel.isCheater =
+                data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
     }
     override fun onStart() {
         super.onStart()
@@ -88,10 +127,11 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer =  questionBank[currentIndex].answer
 
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
 
 //        Toast.makeText(this, "LOOOOOL", Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+    var isCheater = false
+    var cCount = 3
+
+
+}
